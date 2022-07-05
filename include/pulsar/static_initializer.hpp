369bf29f921067fac257443c66d69f65fa07f60f
Fluffy <noe.louis-quentin@hotmail.fr>
2022-07-05 12:13:10 +0200

    Fixed static initializer macro Updated version header

--- include/pulsar/static_initializer.hpp
+++ include/pulsar/static_initializer.hpp
@@ -44,7 +44,7 @@ namespace pul
 
 	/// STATIC-INITIALIZER: Macro -> Initializer
 #ifndef PF_DONT_STATIC_INITIALIZE
-#	define pf_static_initializer(_Ty, name) pf_decl_static pf_decl_inline static_initializer<_Ty> initializer_;
+#	define pf_static_initializer(_Ty) pf_decl_static pf_decl_inline static_initializer<_Ty> initializer_;
 #else // ^^^ !PF_DONT_STATIC_INITIALIZE ^^^ / vvv PF_DONT_STATIC_INITIALIZE vvv
 #	define pf_static_initializer(_Ty)
 #endif // PF_DONT_STATIC_INITIALIZE
